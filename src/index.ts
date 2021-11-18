05047e9db3a6544236b4f338fba5a1dbc0891327
linyyyang <linyyyang@tencent.com>
2021-11-19 01:55:44 +0800

    ✨ feat: StatusCodeConvert

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@
  * @Usage: 
  * @Author: richen
  * @Date: 2021-11-17 09:53:49
- * @LastEditTime: 2021-11-17 09:53:50
+ * @LastEditTime: 2021-11-19 00:54:14
  */
 
-export * from "./Exception";
+export * from "./exception";
+export * from "./code";
